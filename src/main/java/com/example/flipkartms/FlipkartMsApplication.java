5c54c8cde1c3a62312d887b618f4dc249b16746f
Dharrsan Amarnath <dharrsan.amarnath2021@vitstudent.ac.in>
2023-09-17 01:16:03 +0530

    hello

---
+++ src/main/java/com/example/flipkartms/FlipkartMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.flipkartms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipkartMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipkartMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flipkartms/FlipkartMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flipkartms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipkartMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
